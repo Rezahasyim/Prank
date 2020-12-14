b84d469c72fcfdf0167f8592caad04fe7b0081bf
Rezahasyim <75386161+Rezahasyim@users.noreply.github.com>
2020-12-14 22:18:08 +0700

    Create prank.py

---
+++ prank.py
@@ -0,0 +1,287 @@
+#decompiled by PDM31
+
+import os, sys
+
+print '\x1b[1;32mLogin dulu untuk masuk SC nys Ya Guys'
+
+print '\x1b[1;32mSilahkan Login '
+
+import os, sys
+
+def wa():
+
+    os.system('xdg-open https://api.whatsapp.com/send?phone=+6281221653540&text=Assalamualaikum')
+
+def restart():
+
+    ngulang = sys.executable
+
+    os.execl(ngulang, ngulang, *sys.argv)
+
+user = raw_input('ID: ')
+
+import getpass
+
+sandi = raw_input('Password: ')
+
+if sandi == 'prank' and user == 'virus':
+
+    print 'Anda Telah Login'
+
+    sys.exit
+
+else:
+
+    print 'Login GAGAL, Silahkan hubungi ADMIN'
+
+    wa()
+
+    restart()
+
+import os, sys, time, datetime, random, hashlib, re, threading, json, getpass, urllib
+
+from multiprocessing.pool import ThreadPool
+
+try:
+
+    import mechanize
+
+except ImportError:
+
+    os.system('pip2 install mechanize')
+
+else:
+
+    try:
+
+        import requests
+
+    except ImportError:
+
+        os.system('pip2 install requests')
+       time.slepp(3)
+
+time.sleep(0.5)
+
+print "Lo Anjing"
+
+time.sleep(0.5)
+
+print "Lo Anjing"
+
+time.sleep(0.5)
+
+print "Lo Anjing"
+
+time.sleep(0.5)
+
+print "Lo Anjing"
+
+time.sleep(0.5)
+
+print "Lo Anjing"
+
+time.sleep(0.5)
+
+print "Lo Anjing"
+
+time.sleep(0.5)
+
+print "Lo Anjing"
+
+time.sleep(0.5)
+
+print "Lo Anjing"
+
+time.sleep(0.5)
+
+print "Lo Anjing"
+
+time.sleep(0.5)
+
+print "Lo Anjing"
+
+time.sleep(0.5)
+
+print "Lo Anjing"
+
+time.sleep(0.5)
+
+print "Lo Anjing"
+
+time.sleep(0.5)
+
+print "Lo Anjing"
+
+time.sleep(0.5)
+
+print "Lo Anjing"
+
+time.sleep(0.5)
+
+print "Lo Anjing"
+
+time.sleep(0.5)
+
+print "Lo Anjing"
+
+time.sleep(0.5)
+
+print "Lo Anjing"
+
+time.sleep(0.5)
+
+print "Lo Anjing"
+
+time.sleep(0.5)
+
+print "Lo Anjing"
+
+time.sleep(0.5)
+
+print "Lo Anjing"
+
+time.sleep(0.5)
+
+print "Lo Anjing"
+
+time.sleep(0.5)
+
+print "Lo Anjing"
+
+time.sleep(0.5)
+
+print "Lo Anjing"
+
+time.sleep(0.5)
+
+print "Lo Anjing"
+
+time.sleep(0.5)
+
+print "Lo Anjing"
+
+time.sleep(0.5)
+
+print "Lo Anjing"
+
+time.sleep(0.5)
+
+print "Lo Anjing"
+
+time.sleep(0.5)
+
+print "Lo Anjing"
+
+time.sleep(0.5)
+
+print "Lo Anjing"
+
+time.sleep(0.5)
+
+print "Lo Anjing"
+
+time.sleep(0.5)
+
+print "Lo Anjing"
+
+time.sleep(0.5)
+
+print "Lo Anjing"
+
+time.sleep(0.5)
+
+print "Lo Anjing"
+
+time.sleep(0.5)
+
+print "Lo Anjing"
+
+time.sleep(0.5)
+
+print "Lo Anjing"
+
+time.sleep(0.5)
+
+print "Lo Anjing"
+
+time.sleep(0.5)
+
+print "Lo Anjing"
+
+time.sleep(0.5)
+
+print "Lo Anjing"
+
+time.sleep(0.5)
+
+print "Lo Anjing"
+
+time.sleep(0.5)
+
+print "Lo Anjing"
+
+time.sleep(0.5)
+
+print "Lo Anjing"
+
+time.sleep(0.5)
+
+print "Lo Anjing"
+
+time.sleep(0.5)
+
+print "Lo Anjing"
+
+time.sleep(0.5)
+
+print "Lo Anjing"
+
+time.sleep(0.5)
+
+print "Lo Anjing"
+
+time.sleep(0.5)
+
+print "Lo Anjing"
+
+time.sleep(0.5)
+
+print "Lo Anjing"
+
+time.sleep(0.5)
+
+print "Lo Anjing"
+
+time.sleep(0.5)
+
+print "Lo Anjing"
+
+time.sleep(0.5)
+
+print "Lo Anjing"
+
+time.sleep(0.5)
+
+print "Lo Anjing"
+
+time.sleep(0.5)
+
+print "Lo Anjing"
+
+time.sleep(0.5)
+
+print "Lo Anjing"
+
+time.sleep(0.5)
+
+print "Lo Anjing"
+
+time.sleep(0.5)
+
+print "Lo Anjing"
+
+time.sleep(0.5)
+
+print "Lo Anjing"
+
